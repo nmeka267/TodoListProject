5111c2c2a1e33f32221635690a6137997f8b5492
Meka <nmeka@deloitte.com>
2020-01-29 16:49:57 +0530

    inputgropup

--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+import { createDecipher } from "crypto";
 
 export default class Todoinput extends Component {
   render() {
-    return <div>Hello from Today item</div>;
+    const { item, handleChange, handleSubmit, editItems } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="n">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book"></i>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
